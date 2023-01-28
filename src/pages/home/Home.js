0b35ba424f26ae50a50ba4556c6a86488f16efd2
Julieta Bachur <julietabachur@hotmail.com>
2023-01-28 18:25:07 -0300

    arreglos

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="my-5">
+    <div className="my-5 portada">
       <div className="d-flex justify-content-center mx-5 my-5 h-3">
-        <p>“El acto más valiente es pensar <span className='fw-bold fst-italic'>por una misma</span>. Y en voz alta”.</p>
+        <p className="titulo">“El acto <span className='fw-bold fst-italic'>más valiente</span> es pensar por<span className='fw-bold text-decoration-underline'> una misma</span>. Y en voz alta”.</p>
       </div>
-      <div>
+      <div className='d-flex justify-content-end'>
         <p>Coco Chanel</p>
       </div>
     </div>
