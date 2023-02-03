6145c71682d7e83720eba875d9c7393b64ecf57e
Julieta Bachur <julietabachur@hotmail.com>
2023-02-02 22:35:13 -0300

    arreglo navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,34 +4,26 @@ import './navBar.sass'
 
 const NavBar = () => {
   return (
-    
-    <nav className="navbar navbar-expand-lg navbar-light p-3 bg-body-tertiary">
-      <div className="container-fluid">
-        <Link to={'/home'} className="navbar-brand fw-bold text-white" href="#">Blooma</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-          <ul className="navbar-nav ">
-            <li>
-              <Link to={'/home'} className="text-decoration-none text-white mx-4 px-2 active nav" aria-current="page">Home</Link>
-            </li>
-            <li><Link to={'/servicios'} className="text-decoration-none text-white mx-4 px-2 nav">Servicios</Link>
-            </li>            
-            <li>
+
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light p-3">
+        <div class="container-fluid">
+          <Link to={'/home'} className="navbar-brand fw-bold text-white">Blooma</Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link to={'/home'} className="text-decoration-none  text-white mx-4 px-2 active nav" aria-current="page">Home</Link>
+              <Link to={'/servicios'} className="text-decoration-none text-white mx-4 px-2 nav">Servicios</Link>
               <Link to={'/productos'} className="text-decoration-none text-white mx-4 px-2 nav">Productos</Link>
-            </li>            
-            <li>
               <Link to={'/contacto'} className="text-decoration-none text-white mx-4 px-2 nav">Contacto</Link>
-            </li>            
-            <li>
-              <Link to={'/faqs'} className="text-decoration-none text-white mx-4 px-2 nav">FAQS</Link>
-            </li>            
-          </ul>
+              <Link to={'/faqs'} className="text-decoration-none  text-white mx-4 px-2 nav">FAQS</Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
-    
+      </nav>
+    </header>
   )
 }
 
