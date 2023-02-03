6666ffb00a605346dd2b02bef9adad9859addf27
Julieta Bachur <julietabachur@hotmail.com>
2023-02-02 23:39:51 -0300

    agrego mapa y hover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
       <footer className="py-5 px-5">
         <div className="flex-column align-content-center">
           <div className="align-items-center">
-            <h5><Link to={'/faqs'} className="nav-link text-white d-flex justify-content-center">PREGUNTAS FRECUENTES</Link></h5>
+            <h5 className="contacto"><Link to={'/faqs'} className="nav-link text-white d-flex justify-content-center contacto">PREGUNTAS FRECUENTES</Link></h5>
           </div>
           <div className="align-items-center d-flex justify-content-center my-3" >
             <img alt='' src='/img/Rectangulo.svg'/>
@@ -27,37 +27,36 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div className="d-flex justify-content-center py-4 my-4 border-top flex-column">
+        <div className="d-flex justify-content-center pt-4 mt-4 border-top flex-column">
         <div className="align-items-center justify-content-center flex-column">
             <h5 className=' d-flex text-white justify-content-center'>¡ESTAMOS EN CONTACTO!</h5>
-            <ul className="justify-content-center d-flex align-content-center">
+            <ul className="justify-content-center d-flex align-content-center me-5">
               <li className="m-4 align-items-center d-flex text-white column justify-content-center">
                 <a href="https://www.instagram.com/blooma.lashnails/" target="_blank" className="p-0 flex-column text-white text-decoration-none d-flex flex-wrap">
                   <div className='d-flex justify-content-center m-2'>
-                    <FiInstagram size='2.2em'/>
+                    <FiInstagram size='2.2em' className="contacto"/>
                   </div>
-                <div>INSTAGRAM</div>
+                <div className="contacto">INSTAGRAM</div>
                 </a>
               </li>
               <li className="m-4 align-items-center d-flex text-white column justify-content-center">
                 <a href="https://wa.me/541123886544?text=Hola!%20Estoy%20interesada,%20me%20pasas%20m%C3%A1s%20info%20por%20favor?" target="_blank" className="p-0 flex-column text-white text-decoration-none align-content-center d-flex flex-wrap">
                   <div className='d-flex justify-content-center m-2'> 
-                    <FaWhatsapp size='2.5em'/>
+                    <FaWhatsapp size='2.5em' className="contacto"/>
                   </div>
-                <div>WHATSAPP</div>
+                <div className="contacto">WHATSAPP</div>
                 </a>
               </li>
               <li className="m-4 align-items-center d-flex text-white column justify-content-center">
                 <a href = "mailto:blooma.beautystudio@gmail.com" target="_blank" className="p-0 flex-column text-white text-decoration-none d-flex flex-wrap">
                   <div className='d-flex justify-content-center m-2'>
-                    <TfiEmail size='2.2em'/>
+                    <TfiEmail size='2.2em' className="contacto"/>
                   </div>
-                <div>E-MAIL</div>
+                <div className="contacto">E-MAIL</div>
                 </a>
               </li>
             </ul>
           </div>
-          <p className='text-white'>&copy; 2022 Company, Inc. All rights reserved.</p>
         </div>
       </footer>
     </div>  
--- src/pages/contacto/Contacto.js
+++ src/pages/contacto/Contacto.js
@@ -13,47 +13,51 @@ const Contacto = () => {
       </div>     
       <div className="bg-light ">
         <div className="mx-5 d-flex flex-column">
-          <ul className="justify-content-center d-flex align-content-center">
-            <li className="m-4 align-items-center d-flex  column justify-content-center">
-              <a href="https://www.instagram.com/blooma.lashnails/" target="_blank" className="p-0 flex-column text-black text-decoration-none d-flex flex-wrap">
-                <div className='d-flex justify-content-center m-2'>
-                  <FiInstagram size='2.2em'/>
-                </div>
-              <div>INSTAGRAM</div>
-              </a>
-            </li>
-            <li className="m-4 align-items-center d-flex column justify-content-center">
-              <a href="https://wa.me/541123886544?text=Hola!%20Estoy%20interesada,%20me%20pasas%20m%C3%A1s%20info%20por%20favor?" target="_blank" className="p-0 flex-column text-black text-decoration-none align-content-center d-flex flex-wrap">
-                <div className='d-flex justify-content-center m-2'> 
-                  <FaWhatsapp size='2.5em'/>
-                </div>
-              <div>WHATSAPP</div>
-              </a>
-            </li>
-            <li className="m-4 align-items-center d-flex  column justify-content-center">
-              <a href = "mailto:blooma.beautystudio@gmail.com" target="_blank" className="p-0 flex-column text-black text-decoration-none d-flex flex-wrap">
-                <div className='d-flex justify-content-center m-2'>
-                    <BsFacebook size='2.5em'/>      
-                </div>
-                <div>FACEBOOK</div>
-              </a>
-            </li>
-          </ul>
-          <div className="border-dark border-top d-flex justify-content-center align-content-center flex-column py-5">
-            <h2 className="justify-content-start d-flex align-content-center fw-bold">¡Acá estamos!</h2>
-            <div className="justify-content-start d-flex align-content-center flex-column">
+          <div className="d-flex justify-content-center">
+            <ul className="justify-content-around d-flex align-content-center m-0 p-0 flex-wrap">
+              <li className="m-4 align-items-center d-flex  column justify-content-center px-5">
+                <a href="https://www.instagram.com/blooma.lashnails/" target="_blank" className=" p-0 flex-column text-black text-decoration-none d-flex flex-wrap">
+                  <div className='d-flex justify-content-center m-2'>
+                    <FiInstagram size='4em' className="contacto"/>
+                  </div>
+                <div className="contacto">INSTAGRAM</div>
+                </a>
+              </li>
+              <li className="m-4 align-items-center d-flex column justify-content-center px-5">
+                <a href="https://wa.me/541123886544?text=Hola!%20Estoy%20interesada,%20me%20pasas%20m%C3%A1s%20info%20por%20favor?" target="_blank" className=" p-0 flex-column text-black text-decoration-none align-content-center d-flex flex-wrap">
+                  <div className='d-flex justify-content-center m-2'> 
+                    <FaWhatsapp size='4em' className="contacto"/>
+                  </div>
+                <div  className="contacto">WHATSAPP</div>
+                </a>
+              </li>
+              <li className="m-4 align-items-center d-flex  column justify-content-center px-5">
+                <a href = "mailto:blooma.beautystudio@gmail.com" target="_blank" className="  p-0 flex-column text-black text-decoration-none d-flex flex-wrap">
+                  <div className='d-flex justify-content-center m-2'>
+                      <BsFacebook size='4em' className="contacto"/>      
+                  </div>
+                  <div className="contacto">FACEBOOK</div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="border-dark border-top d-flex justify-content-center align-content-center flex-column pt-5 pb-2">
+            <h2 className="justify-content-start d-flex align-content-center fw-bold mb-3">¡Acá estamos!</h2>
+            <div className="justify-content-start d-flex align-content-center flex-column mb-3">
               <p className="justify-content-start d-flex align-content-center mb-0">Nos podes encontrar en</p>
               <p className="justify-content-start d-flex align-content-center">Islandia 26 entre Avenida General Hornos y Darwin.</p>
             </div>
-            <div className="justify-content-start d-flex align-content-center flex-column">
+            <div className="justify-content-start d-flex align-content-center flex-column mb-3">
               <p className="justify-content-start d-flex align-content-center mb-0">Martes a viernes  de 14 a 20 hs.</p>
               <p className="justify-content-start d-flex align-content-center">Sábados de 10 a 16 hs.</p>
             </div>
           </div>
-          <div className="justify-content-center d-flex align-content-center border border-dark">mapa</div>
-          <div>
-            <h3 className="fw-bold justify-content-center d-flex align-content-center">¡Te esperamos!</h3>
-            <p className="justify-content-center d-flex align-content-center">Blooma</p>
+          <div className="justify-content-center d-flex align-content-center mb-5">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3279.739525585919!2d-58.429784199999986!3d-34.711749199999986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccdb2bbd55675%3A0x59a14e48dea5c468!2sCAB%2C%20Islandia%2026%2C%20B1821%20Villa%20Fiorito%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1675391302495!5m2!1ses-419!2sar" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div className=" mb-5">
+            <h3 className="fw-bold justify-content-center d-flex align-content-center fs-2">¡Te esperamos!</h3>
+            <p className="justify-content-center d-flex align-content-center fs-3">Blooma</p>
           </div>
         </div>
       </div>
